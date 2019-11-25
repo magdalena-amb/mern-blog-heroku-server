e5f7ebe56447cd16bbee038548fe8e422483d093
Magda Ambrozy <magda.ambrozy@gmail.com>
2019-11-25 15:05:14 +0100

    Modify server.js

--- server.js
+++ server.js
@@ -36,8 +36,10 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(helmet());
 app.use('/api', postRoutes);
+
+
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '/../client/build')));
+//app.use(express.static(path.join(__dirname, '/../client/build')));
 
 // sanitize
 // app.use(sanitize());
@@ -46,9 +48,9 @@ app.use(express.static(path.join(__dirname, '/../client/build')));
 //     res.send(cleaned);
 // });
 
- app.get('*', (req, res) => {
-     res.sendFile(path.join(__dirname + '/../client/build/index.html'));
-   });
+//  app.get('*', (req, res) => {
+//      res.sendFile(path.join(__dirname + '/../client/build/index.html'));
+//    });
 
 app.listen(config.PORT, () => {
     console.log('Server is running on port:', config.PORT );
